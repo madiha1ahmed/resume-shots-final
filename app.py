f3a4b78fa58ac29f48edf63ba3c7e4cf0358dd2a
madiha1ahmed <56754113+madiha1ahmed@users.noreply.github.com>
2025-12-13 12:04:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -512,6 +512,6 @@ def view_resume(filename):
 
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     #socketio.run(app,debug=True)
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    s#ocketio.run(app, debug=True, host='0.0.0.0', port=5000)
